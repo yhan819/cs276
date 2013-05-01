2b335c582dd4451b8676cf57c9b96c2620574498
Yoon-Suk Han <yhan819@myth14.Stanford.EDU>
2013-05-01 01:34:10 -0700

    first commit

---
+++ README.markdown
@@ -0,0 +1,19 @@
+Programming Assignment 2 starter code
+Query spelling correction
+
+for CS 276, Spring 2012, Stanford
+
+buildmodels.sh and runcorrector.sh form the interface
+
+Do not modify either buildmodels.sh or runcorrector.sh.
+Please edit corrector.py and models.py.  Feel free to add your own files
+if you wish.
+
+Note: 
+
+1. Edit the main functions of both corrector.py and models.py to implement
+the interface in the .sh files. 
+
+2. In correctory.py, the method read_query_data() is simply an example
+showing how to read the query files.  You may need to modify this
+function if you choose to use it. 
---
+++ corrector.py
@@ -0,0 +1,31 @@
+
+import sys
+
+queries_loc = 'data/queries.txt'
+gold_loc = 'data/gold.txt'
+google_loc = 'data/google.txt'
+
+alphabet = "abcdefghijklmnopqrstuvwxyz0123546789&$+_' "
+
+def read_query_data():
+  """
+  all three files match with corresponding queries on each line
+  NOTE: modify the signature of this method to match your program's needs
+  """
+  queries = []
+  gold = []
+  google = []
+  with open(queries_loc) as f:
+    for line in f:
+      queries.append(line.rstrip())
+  with open(gold_loc) as f:
+    for line in f:
+      gold.append(line.rstrip())
+  with open(google_loc) as f:
+    for line in f:
+      google.append(line.rstrip())
+  assert( len(queries) == len(gold) and len(gold) == len(google) )
+  return (queries, gold, google)
+
+if __name__ == '__main__':
+  print(sys.argv)
