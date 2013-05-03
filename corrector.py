8e1f4a9e3448864d9607627279e716069e003716
Yoon-Suk Han <yhan819@stanford.edu>
2013-05-02 22:33:23 -0700

    first draft

--- corrector.py
+++ corrector.py
@@ -14,6 +14,9 @@ pe1 = 0.01
 pqisr = 0.95
 #interpolation lambda
 lamb = 0.2
+jac = 0.7
+
+cache = {}
 
 def unserialize_data(fname):
   with open(fname, 'rb') as f:
@@ -39,20 +42,53 @@ def read_query_data(qry_file):
   #assert( len(queries) == len(gold) and len(gold) == len(google) )
   return (queries, gold, google)
 
-def get_1edit_word(word):
+def get_bigram(word):
+  if len(word) == 1:
+    return set(word)
+  s = set()
+  for i in range(0, len(word)-1):
+    s.add(word[i:i+2])
+  return s
+
+def get_1edit_word(data, word):
+  if word in cache:
+    return cache[word]
+  uni_dict = data[1]
   ans = []
-  ans.append(word)
+  if word in uni_dict:
+    ans.append(word)
+  
+  word_bi = get_bigram(word) 
+  
   for i in range(0,len(word)):
     #insertion
     for a in alphabet:
-      ans.append(word[0:i] + a + word[i:])
+      ins = word[0:i] + a + word[i:]
+      if ins in uni_dict:
+        ins_bi = get_bigram(ins)
+        if 1.0 * len(word_bi&ins_bi)/len(word_bi|ins_bi) > jac and len(ans) < 7:
+          ans.append(ins)
+      #substitution
       if a != word[i]:
-        #substitution
-        ans.append(word[0:i] + a + word[i+1:]) 
+        subs = word[0:i] + a + word[i+1:]
+        if subs in uni_dict:
+          sub_bi = get_bigram(subs)
+          if 1.0 * len(word_bi&sub_bi)/len(word_bi|sub_bi) > jac and len(ans) < 7:
+            ans.append(subs) 
     #deletion
-    ans.append(word[0:i] + word[i+1:])
+    dele = word[0:i] + word[i+1:]
+    if dele in uni_dict:
+      dele_bi = get_bigram(dele)
+      if 1.0 * len(word_bi&dele_bi)/len(word_bi|dele_bi) > jac and len(ans) < 7:
+        ans.append(dele)
     if i < len(word) - 1:
-      ans.append(word[0:i] + word[i+1] + word[i] + word[i+1:])
+      trans = word[0:i] + word[i+1] + word[i] + word[i+1:]
+      if trans in uni_dict:
+        trans_bi = get_bigram(trans)
+        if 1.0 * len(word_bi&trans_bi)/len(word_bi|trans_bi) > jac and len(ans) < 7:
+          ans.append(trans)
+
+  cache[word] = ans
   return ans
 
 def generate_cand(data, query):
@@ -62,30 +98,31 @@ def generate_cand(data, query):
   look at bigram, if it doesn't exist, generate 1-edit words
   '''
   cand = []
+  rst = []
   words = query.split()
   fixed = False
-  for i in range(1,len(words)):
-    if (words[i-1], words[i]) not in bi_dict:
-      fixed = True
-      if i == 1:
-        org = words[i-1]
-        edit_1 = get_1edit_word(words[i-1])
-        for c in edit_1:
-          if c in uni_dict:
-            words[i-1] = c
-            cand.append(' '.join(words))
-        words[i-1] = org
-       
-      org = words[i]
-      edit_1 = get_1edit_word(words[i])
-      for c in edit_1:
-        if c in uni_dict:
-          words[i] = c
-          cand.append(' '.join(words))
-      words[i] = org
-  if not fixed:
-    cand.append(query)
-  return cand
+  for i in range(0,len(words)):
+    org = words[i]
+    cand.append(get_1edit_word(data, words[i]))
+  print >> sys.stderr, cand
+  sent = []
+  for i in range(0,len(cand)):
+    if i == 0:
+      for w in cand[0]:
+        sent.append([w])
+    else:
+      length = len(sent)
+      for m in range(0,length):
+        s = sent.pop(0)
+        lens = len(s)
+        for w in cand[i]:
+          s.insert(lens,w)
+          if i < len(cand) - 1:
+            sent.append(s[:])
+          else:
+            rst.append(' '.join(s[:]))
+          s.pop()
+  return rst
 
 def get_pq(data, candidate):
   num_term = data[0]
@@ -93,8 +130,6 @@ def get_pq(data, candidate):
   bi_dict = data[2]
   words = candidate.split()
   # adding P(w1)
-  if words[0] not in uni_dict:
-    return 0
   score = math.log(float(uni_dict[words[0]]) / num_term)
   
   for i in range(1,len(words)):
@@ -102,8 +137,6 @@ def get_pq(data, candidate):
       score += math.log(lamb * float(uni_dict[words[i]]) / num_term + (1-lamb)* \
         float(bi_dict[(words[i-1],words[i])]) / uni_dict[words[i-1]])
     else:
-      if words[i] not in uni_dict:
-        return 0
       score += math.log(lamb * float(uni_dict[words[i]]) / num_term)
   return score   
   
@@ -144,6 +177,7 @@ def correct_uniform(data, queries, gold, google):
   num_cor = 0
   
   for i in range(0, len(queries)):
+    print >> sys.stderr, i
     candidates = generate_cand(data, queries[i])   
     cand = get_best_cand(data, queries[i], candidates, True)
     if cand == gold[i]:
